a081890aef89e55e576ddad4a2e2e85ed3cefe95
FadhilahAfif <fadhilahafif257@gmail.com>
2025-11-25 19:12:02 +0700

    allow user access

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,22 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+        // Or restrict to specific emails:
+        // return str_ends_with($this->email, '@nexus.com');
+    }
 
     // Karena di AppServiceProvider udah di-unguard, ini sebenernya opsional.
     // Tapi kita taruh aja buat jaga-jaga.
